894bd9cd8cdd0d541abe19e27f1f392542236ebe
candrepa1 <candrepa1@gmail.com>
2021-12-12 13:33:12 -0500

    implementing findOneAndUpdate

--- myApp.js
+++ myApp.js
@@ -84,8 +84,15 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
 	const ageToSet = 20;
-
-	done(null /*, data*/);
+	Person.findOneAndUpdate(
+		{ name: personName },
+		{ age: 20 },
+		{ new: true },
+		(err, person) => {
+			if (err) return console.error(err);
+			done(null, person);
+		}
+	);
 };
 
 const removeById = (personId, done) => {

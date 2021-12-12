d69c613f978b5b66e85356794250bbb655a105a5
candrepa1 <candrepa1@gmail.com>
2021-12-12 12:35:42 -0500

    looked for an specific document using Model.findOne

--- myApp.js
+++ myApp.js
@@ -58,7 +58,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-	done(null /*, data*/);
+	Person.findOne({ favoriteFoods: food }, (err, docs) => {
+		if (err) return console.error(err);
+		done(null, docs);
+	});
 };
 
 const findPersonById = (personId, done) => {
